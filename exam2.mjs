298f1d281ed58019215fbc453754c1c9f4fe2ee1
idaspa <idahelenhal@msn.com>
2025-02-18 15:33:01 +0100

    first rask

---
+++ exam2.mjs
@@ -0,0 +1,33 @@
+// Task 1.
+
+// function that returns the square of a number
+// function that returns a length in mm assuming it has been given a length in inches. 
+// a function that returns the root of a number
+// a function that returns the cube of a number
+
+
+
+
+
+
+
+
+// A function that returns the area of a cirkle given radius.
+
+const PI = 3.14;
+    let r = 4;
+
+function areaOfCirkle(radius) {
+    return (PI * r * r);
+}
+
+console.log(areaOfCirkle(r))
+
+// function that returns a greeting, given a name. 
+
+function greet() {
+    let greeting = "Error, The Code you are trying to reach have to many Syntax Errors, please try again later. ";
+    return greeting;
+}
+
+console.log(greet())
