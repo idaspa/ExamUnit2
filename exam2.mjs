ccfa4599dc5748dcb2c027157296de346e7ba82c
idaspa <idahelenhal@msn.com>
2025-02-18 19:08:49 +0100

    Function cube

--- exam2.mjs
+++ exam2.mjs
@@ -1,33 +1,44 @@
 // Task 1.
 
+
 // function that returns the square of a number
+function squareOfNumber(number) {
+    return number * number;
+}
+console.log(squareOfNumber(7))
+
+
 // function that returns a length in mm assuming it has been given a length in inches. 
+function fromInchesToMm(number) {
+    const one_Inch_In_Mm = 25.4;
+    return number * one_Inch_In_Mm;
+}
+console.log(fromInchesToMm(3))
+
 // a function that returns the root of a number
+
+
+
+
 // a function that returns the cube of a number
-
-
-
-
-
-
+function cubeOfNumber(number) {
+    return number * number * number
+}
+console.log(cubeOfNumber(2))
 
 
 // A function that returns the area of a cirkle given radius.
 
-const PI = 3.14;
-    let r = 4;
-
-function areaOfCirkle(radius) {
+function areaOfCirkle(r) {
+    const PI = 3.14;
     return (PI * r * r);
 }
-
-console.log(areaOfCirkle(r))
+console.log(areaOfCirkle(4))
 
 // function that returns a greeting, given a name. 
-
 function greet() {
     let greeting = "Error, The Code you are trying to reach have to many Syntax Errors, please try again later. ";
     return greeting;
 }
+console.log(greet());
 
-console.log(greet())
